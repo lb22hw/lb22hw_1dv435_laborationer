6f13f90ad65f54f56c76fb33d4bbacf9a9975a50
lb22hw <lb22hw@student.lnu.se>
2014-01-08 14:58:42 +0100

    L05

--- pages/L05.js
+++ pages/L05.js
@@ -1,75 +1,94 @@
+alert("hej");
 
-//var link = document.querySelector("#table .hide a");
-//link.onclick = function(){
+//Hide and show
+var link = document.getElementById("button");
+link.onclick = hideorshow;
 
-	//if(hidden.display === "block"){
-
-	//}
-	//else{
-
-	//}
-//}
-
+var hide = document.querySelector=(".hide");
+function hideoreshow(){
+	if(hide.style.display==="none"){
+		hide.style.display = "block";
+}
+	else{
+		hide.style.display = "none";
+	}
+}
 
 //Valutakonverterare
 var count = 0
 var sek = document.querySelector=("#sek");
 function validateInput(){
-if(sek.value.length > 0)
-		sek.className = 'correct';
+if(sek.value.length > 0){
+		sek.className = "correct";
 	} 
 	else {
-		sek.className = 'empty';
+		sek.className = "empty";
 	}
-	if (document.querySelector('.empty')) {
+	if (document.querySelector("empty")) {
 		
 		alert("Var vänlig fyll i antal kronor i siffror");
 	} 
 	else {
 		function convert(sek){
-			var euro = sek * 9,84;
-			var dollar = sek * 6,54;
+			var euro = (sek * 9,84);
+			var dollar = (sek * 6,54);
 			addMoney(euro.value, dollar.value);
 		}
+	}
 		function addMoney(sek, euro, dollar) {
-	var table = document.querySelector('#tbody');
-	var tr = document.createElement('tr');
-	var sekTd = document.createElement('td');
-	var euroTd = document.createElement('td');
-	var dollarTd = document.createElement('td');
-	
-	
-	var sekTextNode = document.createTextNode(sek);
-	var euroTextNode = document.createTextNode(euro);
-	var dollarTextNode = document.createTextNode(dollar);
-	sekTd.appendChild(sekTextNode);
-	tr.appendChild(sekTd);
-	euroTd.appendChild(euroTextNode);
-	tr.appendChild(euroTd);
-	dollarTd.appendChild(dollarTextNode);
-	tr.appendChild(dollarTd);
-	
-	table.insertBefore(tr, table.firstChild);
-	count++;
-}
+			var table = document.querySelector("#tbody");
+			var tr = document.createElement("tr");
+			var sekTd = document.createElement("td");
+			var euroTd = document.createElement("td");
+			var dollarTd = document.createElement("td");
+			
+			
+			var sekTextNode = document.createTextNode(sek);
+			var euroTextNode = document.createTextNode(euro);
+			var dollarTextNode = document.createTextNode(dollar);
+			sekTd.appendChild(sekTextNode);
+			tr.appendChild(sekTd);
+			euroTd.appendChild(euroTextNode);
+			tr.appendChild(euroTd);
+			dollarTd.appendChild(dollarTextNode);
+			tr.appendChild(dollarTd);
+			
+			table.insertBefore(tr, table.firstChild);
+			count++;
+	}
 
 
 window.onload = function() {
-	//Jag letar upp a-taggen som jag har gett klassen
-	//"submitButton" och sparar undan i en variabel.
-	//Jag har då ett paket i den variabeln som är själva knappen.
-	//Det är inte en textsträng av html, utan det är ett paket
-	//med massor av funktionalitet på.
-	var button = document.querySelector(".button");
 	
-	//Jag lägger till en såkallad even listener på knappen
-	//ett event är något som händer. Tillexempel ett klick,
-	//eller att man för muspekaren över något, trycker på
-	//en tangent etc. Här väljer jag att använda eventet
-	//click. Det vill säga när någon klickar på den.
-	//När någon klickar på min länk så säger jag att funktionen
-	//'validateInput' ska köras.
-	button.addEventListener('click', validateInput);
-};
+	var button = document.querySelector(".button");
+	button.addEventListener("click", validateInput);
+}
+
+//Bildspel
+
+var Bild = new Array()
+
+Bild[0] = "../css/pics/ylva.png"   
+Bild[1] = "../css/pics/ylva2.png"
+Bild[2] = "../css/pics/ylva3.png"
+Bild[3] = "../css/pics/ylva4.png"
+
+
+var f
+var a = 0
+var b = Bild.length
+
+var laddaBild = new Array()
+for (i = 0; i < b; i++){
+   laddaBild[i] = new Image()
+   laddaBild[i].src = Bild[i]
+}
+
+function bildspel(){
+   document.images.bildspel.src = laddaBild[a].src
+   a = a + 1
+   if (a > (b-1)) a=0
+   f = setTimeout("bildspel()", 5000)
+}
 
 
